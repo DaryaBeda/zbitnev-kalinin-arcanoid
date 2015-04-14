db3df00565987d298da5e60131dc8ad7c0b2c19c
DaryaBeda <beda1304@gmail.com>
2015-04-14 20:47:54 +0400

    Добавлены классы физич. представлений объектов

---
+++ src/view/BasicPaddleView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import model.PublishingSprite;
+
+/**
+ *
+ * @author Дарья
+ */
+public class BasicPaddleView extends IngameObjectView{
+
+    public BasicPaddleView(PublishingSprite sprite) throws IOException {
+        super(sprite);
+        setImage(ImageIO.read(new File("default\\gfx\\paddles\\basic.png")));
+    }
+    
+}
