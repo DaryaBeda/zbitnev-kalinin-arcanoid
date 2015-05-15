d36a482725a4eb1a4c80d903aa2878532f38b1e7
DaryaBeda <beda1304@gmail.com>
2015-05-15 13:42:49 +0400

    Ответственность за управление столкновениями передана модели

--- src/model/GameField.java
+++ src/model/GameField.java
@@ -13,7 +13,7 @@ import model.interaction.CollisionListener;
  * @author Nikita Kalinin <nixorv@gmail.com>
  *
  */
-public class GameField implements CollisionListener {
+public class GameField  {
 
     private ArrayList<IngameObject> _objects;
     private Dimension _dimension;
@@ -66,71 +66,5 @@ public class GameField implements CollisionListener {
 
     
 
-    /**
-     * Обработать столкновения
-     *
-     * @param storage Словарь столкновений, где ключ - столкнувшийся объект,
-     * значение - список объектов, с которыми он столкнулся
-     */
-    @Override
-    public void collisionOccured(HashMap<CollidedObject, ArrayList<CollidedObject>> storage) {
-
-        // Вместо объектов, от которых принимается эффект (активные)
-        // передаётся их копия до начала обработки вообще всех столкновений
-        HashMap<CollidedObject, ArrayList<CollidedObject>> storage_copy = deepCopyStorage(storage);
-
-        Iterator<CollidedObject> i, copyi, j, copyj;
-        i = storage.keySet().iterator();
-        copyi = storage_copy.keySet().iterator();
-
-        while (i.hasNext() && copyi.hasNext()) {
-
-            CollidedObject obj1 = i.next();
-            obj1.object().setPosition(obj1.object().getPosition());
-            CollidedObject obj1copy = copyi.next();
-            obj1copy.object().setPosition(obj1copy.object().getPosition());
-            j = storage.get(obj1).iterator();
-            copyj = storage_copy.get(obj1copy).iterator();
-
-            while (j.hasNext() && copyj.hasNext()) {
-
-                CollidedObject obj2 = j.next();
-                obj2.object().setPosition(obj2.object().getPosition());
-                CollidedObject obj2copy = copyj.next();
-                obj2copy.object().setPosition(obj2copy.object().getPosition());
-                obj1.object().processCollision(obj1, obj2copy);
-                obj2.object().processCollision(obj2, obj1copy);
-            }
-        }
-    }
-
-    /**
-     * Порождает копию словаря коллизии вместе со всеми хранимыми объектами
-     *
-     * @param storage Словарь коллизии
-     * @return Копия словаря коллизии
-     */
-    private HashMap<CollidedObject, ArrayList<CollidedObject>> deepCopyStorage(
-            HashMap<CollidedObject, ArrayList<CollidedObject>> storage) {
-
-        HashMap<CollidedObject, ArrayList<CollidedObject>> deepcopy = new HashMap<>();
-
-        try {
-
-            for (CollidedObject key : storage.keySet()) {
-
-                CollidedObject key_copy = (CollidedObject) key.clone();
-                ArrayList<CollidedObject> values_copy = new ArrayList<>();
-                
-                for (CollidedObject obj : storage.get(key)) {
-                    values_copy.add((CollidedObject) obj.clone());
-                }
-
-                deepcopy.put(key_copy, values_copy);
-            }
-        } catch (CloneNotSupportedException exc) {
-        }
-
-        return deepcopy;
-    }
+    
 }
