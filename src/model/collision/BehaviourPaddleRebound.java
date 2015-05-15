6af18d80be9787dc8ee63c41582d6fbd7a0f17ac
DaryaBeda <beda1304@gmail.com>
2015-05-15 14:38:21 +0400

    Удалена привязка к объектам в invoke

--- src/model/collision/BehaviourPaddleRebound.java
+++ src/model/collision/BehaviourPaddleRebound.java
@@ -18,58 +18,56 @@ public class BehaviourPaddleRebound extends CollisionBehaviour {
     public void invoke(CollidedObject from, CollidedObject to) {
         IngameObject fromObject = from.getObject();
         IngameObject toObject = to.getObject();
-        if (to.getObject() instanceof Ball) {
-            Speed2D newSpeed = null;
-            to.getObject().setPosition(new Point2D.Double(toObject.getPosition().x,
-                    fromObject.getPosition().y - toObject.getSize().height));
+        Speed2D newSpeed = null;
+        to.getObject().setPosition(new Point2D.Double(toObject.getPosition().x,
+                fromObject.getPosition().y - toObject.getSize().height));
 
-            // Найти два центра расчета вектора.
-            Point2D.Double paddleLeftCenter = new Point2D.Double(fromObject.getPosition().x + (fromObject.getSize().width / 5) * 2, fromObject.getPosition().y);
-            Point2D.Double paddleRightCenter = new Point2D.Double(fromObject.getPosition().x + (fromObject.getSize().width / 5) * 3, fromObject.getPosition().y);
+        // Найти два центра расчета вектора.
+        Point2D.Double paddleLeftCenter = new Point2D.Double(fromObject.getPosition().x + (fromObject.getSize().width / 5) * 2, fromObject.getPosition().y);
+        Point2D.Double paddleRightCenter = new Point2D.Double(fromObject.getPosition().x + (fromObject.getSize().width / 5) * 3, fromObject.getPosition().y);
 
-            // Центр ракетки
-            Point2D.Double paddleCenter = new Point2D.Double(fromObject.getPosition().x + fromObject.getSize().width / 2, fromObject.getPosition().y);
+        // Центр ракетки
+        Point2D.Double paddleCenter = new Point2D.Double(fromObject.getPosition().x + fromObject.getSize().width / 2, fromObject.getPosition().y);
 
             // Относительные координаты центра мяча в декартовой системе координат (точка B).
-            // Считаем, что paddleCenter - это точка A(0, 0).
-            Point2D.Double relBallCenter = new Point2D.Double(toObject.getPosition().x + toObject.getSize().width / 2 - paddleCenter.x,
-                    paddleCenter.y - toObject.getPosition().y - toObject.getSize().height / 2);
+        // Считаем, что paddleCenter - это точка A(0, 0).
+        Point2D.Double relBallCenter = new Point2D.Double(toObject.getPosition().x + toObject.getSize().width / 2 - paddleCenter.x,
+                paddleCenter.y - toObject.getPosition().y - toObject.getSize().height / 2);
 
-            // Если мяч между двумя центрами, направляем вектор вверх.
-            if (relBallCenter.x <= fromObject.getSize().width / 10 && relBallCenter.x >= -fromObject.getSize().width / 10) {
-                newSpeed = new Speed2D(0, -((Ball) toObject).getDefaultSpeedScalar());
-            } else {
+        // Если мяч между двумя центрами, направляем вектор вверх.
+        if (relBallCenter.x <= fromObject.getSize().width / 10 && relBallCenter.x >= -fromObject.getSize().width / 10) {
+            newSpeed = new Speed2D(0, -((Ball) toObject).getDefaultSpeedScalar());
+        } else {
 
-                // В зависимости от трети ракетки, в которой располагается мяч, выбираем центр расчета вектора скорости.
-                Point2D.Double paddleNewCenter = relBallCenter.x > fromObject.getSize().width / 10 ? paddleRightCenter : paddleLeftCenter;
+            // В зависимости от трети ракетки, в которой располагается мяч, выбираем центр расчета вектора скорости.
+            Point2D.Double paddleNewCenter = relBallCenter.x > fromObject.getSize().width / 10 ? paddleRightCenter : paddleLeftCenter;
 
-                // Рассчитываем относительное положение мяча от выбранного центра.
-                relBallCenter = new Point2D.Double(relBallCenter.x + paddleCenter.x - paddleNewCenter.x, relBallCenter.y);
+            // Рассчитываем относительное положение мяча от выбранного центра.
+            relBallCenter = new Point2D.Double(relBallCenter.x + paddleCenter.x - paddleNewCenter.x, relBallCenter.y);
 
-                // Коэффициенты уравнения точки пересечения прямой и окружности.
-                double a = (Math.pow(relBallCenter.x, 2) + Math.pow(relBallCenter.y, 2)) / Math.pow(relBallCenter.x, 2);
-                double b = 0;
-                double c = -Math.pow(((Ball) toObject).getDefaultSpeedScalar(), 2);
+            // Коэффициенты уравнения точки пересечения прямой и окружности.
+            double a = (Math.pow(relBallCenter.x, 2) + Math.pow(relBallCenter.y, 2)) / Math.pow(relBallCenter.x, 2);
+            double b = 0;
+            double c = -Math.pow(((Ball) toObject).getDefaultSpeedScalar(), 2);
 
-                // Дискриминант.
-                double D = Math.pow(b, 2) - 4 * a * c;
+            // Дискриминант.
+            double D = Math.pow(b, 2) - 4 * a * c;
 
-                // Точки пересечения.
-                Point2D.Double p1 = new Point2D.Double((float) ((-b + Math.sqrt(D)) / (2 * a)), 0);
-                Point2D.Double p2 = new Point2D.Double((float) ((-b - Math.sqrt(D)) / (2 * a)), 0);
+            // Точки пересечения.
+            Point2D.Double p1 = new Point2D.Double((float) ((-b + Math.sqrt(D)) / (2 * a)), 0);
+            Point2D.Double p2 = new Point2D.Double((float) ((-b - Math.sqrt(D)) / (2 * a)), 0);
 
-                // Находим y{1,2} у точек.
-                p1.y = p1.x * relBallCenter.y / relBallCenter.x;
-                p2.y = p2.x * relBallCenter.y / relBallCenter.x;
+            // Находим y{1,2} у точек.
+            p1.y = p1.x * relBallCenter.y / relBallCenter.x;
+            p2.y = p2.x * relBallCenter.y / relBallCenter.x;
 
-                // Нужная точка пересечения имеет положительную y-координату.
-                Point2D.Double p = p1.y > 0 ? p1 : p2;
+            // Нужная точка пересечения имеет положительную y-координату.
+            Point2D.Double p = p1.y > 0 ? p1 : p2;
 
                 // Находим горизонтальную и вертикальную сооставляющие вектора скорости.
-                // y отрицательный, чтобы перейти в экранную систему координат.
-                newSpeed = new Speed2D(p.x, -Math.abs(p.y));
-            }
-            to.getObject().setSpeed(newSpeed);
+            // y отрицательный, чтобы перейти в экранную систему координат.
+            newSpeed = new Speed2D(p.x, -Math.abs(p.y));
         }
+        to.getObject().setSpeed(newSpeed);
     }
 }
